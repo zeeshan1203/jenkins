a4b87302cfda47222ccef861a13eb3ecd973ceec
zeeshan1203 <zeeshanshaik1203@gmail.com>
2022-01-05 16:11:08 +0530

    added & updated and modified

---
+++ vars/webcode.groovy
@@ -0,0 +1,20 @@
+def call(String COMPONENT) {
+    pipeline {
+
+        agent any
+
+//    triggers {
+//      pollSCM('H/2 * * * *')
+//    }
+
+        stages {
+
+            stage('Submit Code to SonarQube') {
+                steps {
+                    sh "sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.java.binaries=./target -Dsonar.host.url=http://172.31.17.177:9000 -Dsonar.login=5d0bbdf4d611370d0627732bde2157567539a8af"
+                }
+            }
+
+        } // stages
+    } // pipeline
+}
