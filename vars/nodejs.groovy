378dea659eb712c5f846b06248e912bf2ccc6b00
zeeshan1203 <zeeshanshaik1203@gmail.com>
2022-01-05 15:35:47 +0530

    added & updated and modified

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -9,17 +9,17 @@ def call(String COMPONENT) {
 
         stages {
 
+            stage('Submit Code to SonarQube') {
+                steps {
+                    sh "sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.17.177:9000 -Dsonar.login=5d0bbdf4d611370d0627732bde2157567539a8af"
+                }
+            }
+
             stage('Download Dependencies') {
                 steps {
                     script {
                         sh "npm install"
                     }
-                }
-            }
-
-            stage('Submit Code to SonarQube') {
-                steps {
-                    sh "sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.17.177:9000 -Dsonar.login=5d0bbdf4d611370d0627732bde2157567539a8af"
                 }
             }
 
