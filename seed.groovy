4b0bbd831315b0b4e2c53ba53295c9b32a942f0c
zeeshan1203 <zeeshanshaik1203@gmail.com>
2022-01-05 12:30:16 +0530

    added & updated and modified

---
+++ seed.groovy
@@ -0,0 +1,63 @@
+folder('CI-Pipelines') {
+    displayName('CI-Pipelines')
+    description('CI-Pipelines')
+}
+
+/// Simple Syntax
+
+//pipelineJob('CI-Pipelines/frontend') {
+//  configure { flowdefinition ->
+//    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+//      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+//        'userRemoteConfigs' {
+//          'hudson.plugins.git.UserRemoteConfig' {
+//            'url'('https://zeeshanshaik1203@dev.azure.com/zeeshanshaik1203/Roboshop/_git/frontend')
+//          }
+//        }
+//        'branches' {
+//          'hudson.plugins.git.BranchSpec' {
+//            'name'('*/main')
+//          }
+//        }
+//      }
+//      'scriptPath'('Jenkinsfile')
+//      'lightweight'(true)
+//    }
+//  }
+//}
+
+/// RUnning a loop to lkeep the code dry.
+def component = ["cart", "catalogue","user","payment","shipping","frontend"];
+
+def count=(component.size()-1)
+for (i in 0..count) {
+    def j=component[i]
+    pipelineJob("CI-Pipelines/${j}") {
+        configure { flowdefinition ->
+            flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+                'triggers' {
+                    'hudson.triggers.SCMTrigger' {
+                        'spec'('*/2 * * * *')
+                        'ignorePostCommitHooks'(false)
+                    }
+                }
+            }
+            flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+                'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                    'userRemoteConfigs' {
+                        'hudson.plugins.git.UserRemoteConfig' {
+                            'url'('https://zeeshanshaik1203@dev.azure.com/zeeshanshaik1203/Roboshop/_git/'+j+'frontend')
+                        }
+                    }
+                    'branches' {
+                        'hudson.plugins.git.BranchSpec' {
+                            'name'('*/main')
+                        }
+                    }
+                }
+                'scriptPath'('Jenkinsfile')
+                'lightweight'(true)
+            }
+        }
+    }
+}
