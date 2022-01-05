b212a004575c7d8f781917690ef859fc2b00258a
zeeshan1203 <zeeshanshaik1203@gmail.com>
2022-01-05 11:59:14 +0530

    added & updated and modified

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,3 +1,22 @@
-def download(message) {
-    sh "npm install"
+def call() {
+    pipeline {
+
+        agent any
+
+        triggers {
+            pollSCM('H/2 * * * *')
+        }
+
+        stages {
+
+            stage('Download Dependencies') {
+                steps {
+                    script {
+                        sh "npm install"
+                    }
+                }
+            }
+
+        } // stages
+    } // pipeline
 }
