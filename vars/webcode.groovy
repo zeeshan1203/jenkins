bdce0266d2babf7cf9bfab4b50857dfa83cde415
zeeshan1203 <zeeshanshaik1203@gmail.com>
2022-01-05 19:25:36 +0530

    added & updated and modified

--- vars/webcode.groovy
+++ vars/webcode.groovy
@@ -13,7 +13,7 @@ def call(String COMPONENT) {
                 steps {
                     sh "sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.java.binaries=./target -Dsonar.host.url=http://172.31.17.177:9000 -Dsonar.login=5d0bbdf4d611370d0627732bde2157567539a8af"
                 }
-            }}
+            }
 
         stage('Quality Gate Status') {
             steps {
