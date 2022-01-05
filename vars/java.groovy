7337dcaaa105f199576069bfdb5ca2264f5c6867
zeeshan1203 <zeeshanshaik1203@gmail.com>
2022-01-05 19:30:53 +0530

    added & updated and modified

--- vars/java.groovy
+++ vars/java.groovy
@@ -17,17 +17,17 @@ def call(String COMPONENT) {
                 }
             }
 
-            stage('Submit Code to SonarQube') {
-                steps {
-                    sh "sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.java.binaries=./target -Dsonar.host.url=http://172.31.17.177:9000 -Dsonar.login=5d0bbdf4d611370d0627732bde2157567539a8af"
-                }
-            }
-
-            stage('Quality Gate Status') {
-                steps {
-                    sh "sonar-quality-gate.sh admin admin123 172.31.17.177 ${COMPONENT}"
-                }
-            }
+//            stage('Submit Code to SonarQube') {
+//                steps {
+//                    sh "sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.java.binaries=./target -Dsonar.host.url=http://172.31.17.177:9000 -Dsonar.login=5d0bbdf4d611370d0627732bde2157567539a8af"
+//                }
+//            }
+//
+//            stage('Quality Gate Status') {
+//                steps {
+//                    sh "sonar-quality-gate.sh admin admin123 172.31.17.177 ${COMPONENT}"
+//                }
+//            }
 
             stage('Build Package') {
                 steps {
--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -9,23 +9,32 @@ def call(String COMPONENT) {
 
         stages {
 
-            stage('Submit Code to SonarQube') {
-                steps {
-                    sh "sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.17.177:9000 -Dsonar.login=5d0bbdf4d611370d0627732bde2157567539a8af"
-                }
-            }
-
-            stage('Quality Gate Status') {
-                steps {
-                    sh "sonar-quality-gate.sh admin admin123 172.31.17.177 ${COMPONENT}"
-                }
-            }
+//            stage('Submit Code to SonarQube') {
+//                steps {
+//                    sh "sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.17.177:9000 -Dsonar.login=5d0bbdf4d611370d0627732bde2157567539a8af"
+//                }
+//            }
+//
+//            stage('Quality Gate Status') {
+//                steps {
+//                    sh "sonar-quality-gate.sh admin admin123 172.31.17.177 ${COMPONENT}"
+//                }
+//            }
 
             stage('Download Dependencies') {
                 steps {
                     script {
                         sh "npm install"
                     }
+                }
+            }
+
+            stage('Prepare Archive' ) {
+                steps {
+                    sh """
+            ls
+            zip -r ${COMPONENT}.zip node_modules server.js 
+          """
                 }
             }
 
