568ff09d078c5be25c5d6bde9385452f1c5d8675
zeeshan1203 <zeeshanshaik1203@gmail.com>
2022-01-06 12:25:17 +0530

    added & updated and modified

--- vars/python3.groovy
+++ vars/python3.groovy
@@ -23,10 +23,10 @@ def call(String COMPONENT) {
 
             stage('Prepare Archive' ) {
                 when { expression { sh([returnStdout: true, script: 'echo ${GIT_BRANCH} | grep tags || true' ]) } }
-                script {
-                    addShortText background: 'yellow', color: 'black', borderColor: 'yellow', text: "${GIT_BRANCH}"
-                }
                 steps {
+                    script {
+                        addShortText background: 'yellow', color: 'black', borderColor: 'yellow', text: "${GIT_BRANCH}"
+                    }
                     sh """     
             zip -r ${COMPONENT}-`echo ${GIT_BRANCH}| awk -F / '{print \$NF}'`.zip payment.ini payment.py rabbitmq.py requirements.txt 
           """
